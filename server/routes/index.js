9db794e5f0aac9e9867ae07e62bb24bb6921b7f9
Sergey Fomin <gray7410@yandex.ru>
2022-07-22 16:47:10 +0500

    add token service

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 
+router.use("/auth", require("./auth.routes"));
 router.use("/room", require("./room.routes"));
 router.use("/quality", require("./quality.routes"));
 router.use("/promo", require("./promo.routes"));
