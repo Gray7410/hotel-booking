7a5f76dcccf251d2dc9b04d681ffebb1328a302d
Sergey Fomin <gray7410@yandex.ru>
2022-07-29 12:29:16 +0500

    config docker

--- server/app.js
+++ server/app.js
@@ -16,11 +16,15 @@ app.use("/api", routes);
 
 const PORT = config.get("port") ?? 8080;
 
-// if (process.env.NODE_ENV === "production") {
-//   console.log("Production");
-// } else {
-//   console.log("Development");
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client")));
+
+  const indexPath = path.join(__dirname, "client", "index.html");
+
+  app.get("*", (req, res) => {
+    res.sendFile(indexPath);
+  });
+}
 
 async function start() {
   try {
